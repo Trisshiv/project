4c96e39038559f79c092bcfac07cd16b2f99cfed
Trisshiv T <79263531+Trisshiv@users.noreply.github.com>
2022-05-20 16:38:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ function draw() {
     image(video, 0, 0, 575, 450);
 
     if (status_model != "") {
+        objectDetector.detect(video, gotResult);
+
         for(i = 0; i < object.length; i++) {
             fill('red');
             percent = floor(object[i].confidence * 100)
@@ -33,6 +35,17 @@ function draw() {
             noFill();
             stroke('red');
             rect(object[i].x, object[i].y, object[i].width, object[i].height);
+
+            if(object[0].label == text_input) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("founds").innerHTML = text_input + " found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(text_input + " found");
+                synth.speak(utterThis);
+            } else {
+                document.getElementById("founds").innerHTML = text_input + " not found";
+            }
         }
     }
 }
